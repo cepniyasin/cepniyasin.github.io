5158eb6f3fc8775791a4d5fe3d55a7f9527b6a50
json nerdy <nerdy@ramsay>
2025-01-03 17:44:21 +0100

    take away unused stuff for now and fix about page temporarily

--- cepniyasin-github-io/src/app/about/about.component.ts
+++ cepniyasin-github-io/src/app/about/about.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule
+
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
 
+  onContactClick() {
+    window.open('https://github.com/cepniyasin', '_blank', 'noopener');
+  }
 }
